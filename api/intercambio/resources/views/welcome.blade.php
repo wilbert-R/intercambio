054218496e61e1a50fc4c84a3254a2bba10f88fc
wilbert-R <a21311056@uthermosillo.edu.mx>
2024-01-26 14:00:53 -0700

    Cambios realizados en welcome

---
+++ api/intercambio/resources/views/welcome.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>intercambio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<header id="header" class="container">
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.html">Welcome</a></li>
+									<li>
+										<a href="#">Perfil</a>
+										<ul>
+											<li><a href="#">mi Perfil</a></li>
+											<li><a href="#">Mis Publicaciones</a></li>
+											<li><a href="#">Intercambios</a></li>
+										</ul>
+									</li>
+								</ul>
+							</nav>
+
+					</header>
+				</div>
+
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<div id="banner" class="box container">
+						<div class="row">
+							<div class="col-7 col-12-medium">
+								<h2>Hola Bienvenido.</h2>
+                                <h2>Usuario: wilbert Jesus</h2>
+
+							</div>
+							<div class="col-5 col-12-medium">
+								<ul>
+									<li><a href="#" class="button large icon solid fa-arrow-circle-right">Realizar publicación </a></li>
+									<li><a href="#" class="button alt large icon solid fa-question-circle">Buscar Artículos</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+                
+			<!-- Features -->
+				<div id="features-wrapper">
+					<div class="container">
+                        <h2>Publicaciones Nuevas</h2>
+						<div class="row">
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>Samsung S10</h2>
+												<p>Usuario: Jesus Garcia</p>
+											</header>
+											<p>Intercambio Telefono Samsung S10 por iphone 11 pro.</p>
+                                            <img src="https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" height="200">
+										</div>
+									</section>
+
+							</div>
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>iphone 15</h2>
+												<p>Usuario: Rosita Lopez</p>
+											</header>
+											<p>Intercambio iPhone 15 por un Samsung S23.</p>
+                                            <img src="https://images.unsplash.com/photo-1695048132832-b41495f12eb4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" height="200">
+										</div>
+									</section>
+
+							</div>
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>Motorola razr 40 ultra</h2>
+												<p>Usuario: Luis Escalante</p>
+											</header>
+											<p>Intercambio Motorola razr 40 ultra por una Televisión de 50 pulgadas</p>
+                                            <img src="https://mxmoto.vtexassets.com/arquivos/razr-40-ultra-pdp-display-static-m.jpg" alt="" height="200">
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
